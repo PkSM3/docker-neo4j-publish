28453fe5aaa1bc0b6c64f3790086da7eb02ef9b5
PkSM3 <pokesam3@gmail.com>
2018-03-27 18:42:26 +0200

    no auth 3.3.4 ........

--- 3.3.4/community/docker-entrypoint.sh
+++ 3.3.4/community/docker-entrypoint.sh
@@ -174,6 +174,35 @@ if [ -d /metrics ]; then
     NEO4J_dbms_directories_metrics="/metrics"
 fi
 
+
+
+
+DATA_FILE="panama-papers-mac-2016-06-27.tar.gz"
+if [ ! -f "./$DATA_FILE" ]; then
+  echo "Downloading data"
+  wget "https://cloudfront-files-1.publicintegrity.org/offshoreleaks/neo4j/$DATA_FILE"
+else
+  echo "Not downloading data as file already exists"
+fi
+
+if [ ! -d "./panama-papers" ]; then
+  tar -zxvf "$DATA_FILE"
+fi
+
+if [ ! -d "/data/databases/panama.graphdb" ]; then
+  echo "Copying data over to databases directory"
+  cp -R ./panama-papers/ICIJ\ Panama\ Papers/panama_data_for_neo4j/databases /data/
+else
+  echo "Skipping copying data over to databases directory as panama.graphdb already exists"
+fi
+
+echo "Copying config and plugins"
+cp -R ./panama-papers/ICIJ\ Panama\ Papers/panama_data_for_neo4j/conf/* /var/lib/neo4j/conf/
+mkdir /var/plugins
+cp -R ./panama-papers/ICIJ\ Panama\ Papers/panama_data_for_neo4j/plugins/* /var/plugins/
+
+
+
 # set the neo4j initial password only if you run the database server
 if [ "${cmd}" == "neo4j" ]; then
     if [ "${NEO4J_AUTH:-}" == "none" ]; then
