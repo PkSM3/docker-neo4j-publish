2779dfeb5ddb6219d82a19dcfcaf9a1c0814fd8b
PkSM3 <pokesam3@gmail.com>
2018-03-27 18:16:01 +0200

    no auth 3.3.4 ..

--- 3.3.4/community/docker-entrypoint.sh
+++ 3.3.4/community/docker-entrypoint.sh
@@ -112,7 +112,7 @@ fi
 : ${NEO4J_ha_host_coordination:="$(hostname):5001"}
 : ${NEO4J_ha_host_data:="$(hostname):6001"}
 : ${NEO4J_dbms_security_auth__enabled:="false"}
-: ${NEO4J_causal__clustering_transaction__advertised__address:="$(hostname):7687"}}
+: ${NEO4J_causal__clustering_transaction__advertised__address:="$(hostname):7687"}
 
 
 
