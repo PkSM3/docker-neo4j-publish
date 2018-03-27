aa26d9c1576edad5e1cae3ff53f9277d9d146f78
PkSM3 <pokesam3@gmail.com>
2018-03-27 18:39:55 +0200

    no auth 3.3.4 .......

--- 3.3.4/community/docker-entrypoint.sh
+++ 3.3.4/community/docker-entrypoint.sh
@@ -113,6 +113,7 @@ fi
 : ${NEO4J_ha_host_data:="$(hostname):6001"}
 : ${NEO4J_dbms_security_auth__enabled:="false"}
 : ${NEO4J_dbms_connector_bolt_advertised__address:="$(hostname):7687"}
+: ${NEO4J_dbms_active__database:="panama.graphdb"}
 
 
 
