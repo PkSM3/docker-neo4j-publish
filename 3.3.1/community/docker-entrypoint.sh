ed1c547262d2352948466c8ba00172f28008b9c7
PkSM3 <pokesam3@gmail.com>
2018-03-28 15:47:22 +0200

    wget to Dockerfile 3.3.3

--- 3.3.1/community/docker-entrypoint.sh
+++ 3.3.1/community/docker-entrypoint.sh
@@ -198,27 +198,6 @@ if [ -d /metrics ]; then
     NEO4J_dbms_directories_metrics="/metrics"
 fi
 
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
-# # # # # # # # # # # # # # # # DOWNLOAD: MOVIES DB # # # # # # # # # # # # # 
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
-
-
-echo "Downloading..."
-#wget "https://example-data.neo4j.org/3.0-datasets/cineasts.tgz"
-wget "https://github.com/neo4j-contrib/neo4j-apoc-procedures/releases/download/3.3.0.1/apoc-3.3.0.1-all.jar"
-wget "https://github.com/neo4j-contrib/neo4j-graph-algorithms/releases/download/3.3.0.0/graph-algorithms-algo-3.3.0.0.jar"
-wget "https://products.graphaware.com/download/framework-server-community/graphaware-server-community-all-3.3.1.51.jar"
-wget "https://products.graphaware.com/download/neo4j-to-elasticsearch/graphaware-neo4j-to-elasticsearch-3.3.1.51.7.jar"
-echo "Downloading: OK"
-echo "Uncompressing..."
-#tar -zxvf cineasts.tgz -C data/
-mv *.jar plugins/
-echo "Uncompressing: OK"
-
-
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 # # # # # # # # # # # # # # # # DOWNLOAD: PANAMA PAPERS # # # # # # # # # # # 
